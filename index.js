e6630a5d4de43591b44a29bdd94fdb9d8a942f65
楼教主 <cik520@qq.com>
2016-10-25 16:44:37 +0800

    Completion of basic functions

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 'use strict'
 
+const parse = require('url').parse
 const http = require('http')
+const qs = require('qs')
 
-function curl(url, data, cb) {
-
+const headers = {
+  'Accept': 'text/html',
+  // 'Accept-Encoding': 'gzip, deflate, sdch',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36',
 }
 
-curl.get = (url, cb) => curl(url, null, cb)
-curl.post = (url, data, cb) => curl(url, data, cb)
+function curl(url, data, cb) {
+  let isPOST = typeof cb === 'function'
+
+  if (isPOST) {
+    data = typeof data === 'string' ? data : qs.stringify(data)
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    headers['Content-Length'] = Buffer.byteLength(data)
+  } else {
+    cb = data
+    data = ''
+  }
+
+  let uri = parse(url)
+  let options = {
+    headers,
+    method: isPOST ? 'POST' : 'GET',
+    hostname: uri.hostname,
+    port: uri.port,
+    path: uri.path,
+  }
+
+  let req = http.request(options, res => {
+    let data = []
+    res.on('data', chunk => data.push(chunk))
+    res.on('end', _ => cb(Buffer.concat(data)))
+  })
+
+  req.write(data)
+  req.end()
+
+  return req
+}
 
 module.exports = curl
